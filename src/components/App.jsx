fd33d5f66d17f655dd9477f78726d52bc7ecdb54
syhinin <suhinin@bright.com.ua>
2018-07-18 18:27:20 +0300

    first stage done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,85 +4,75 @@ import {moviesData} from "../moviesData";
 
 
 class MovieItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeBtn: false
-        }
-    }
-    toggleActiveBtn = () =>{
+    state = {
+        activeBtn: false
+    };
+    hendlleWillWatchClick= () =>{
+        this.props.onMovieAdd(this.props.item)
         this.setState({activeBtn: !this.state.activeBtn})
-    }
+    };
     render() {
-        const {data} = this.props;
-        console.log("THIS from item", data);
-        return (
-            <div className="row">
-                {this.props.data.map( (item) =>{
-                    return (
-                        <div className="col-4 mb-4">
-                            <div key={item.id} className="card " style={{width: '100%'}}>
-                                <img className="card-img-top" src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`} alt="Card image cap"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">{item.original_title}</h5>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <p className="mb-0">{item.vote_average}</p>
-                                        <button type="button" onClick={this.toggleActiveBtn} className={`btn ${this.state.activeBtn ? 'btn-success' : 'btn-secondary'}`}>Will Watch</button>
-                                    </div>
-                                </div>
-                            </div>
+        const {item, moviesWillWatch} = this.props;
+        return (   <div className="col-4 mb-4">
+                <div key={item.id} className="card " style={{width: '100%'}}>
+                    <img className="card-img-top" src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`} alt="Card image cap"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{item.original_title}</h5>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <p className="mb-0">{item.vote_average}</p>
+                            <button type="button" onClick={this.hendlleWillWatchClick} className={`btn ${moviesWillWatch.some(movieItem => movieItem.id === item.id)  ? 'btn-success' : 'btn-secondary'}`}>Will Watch</button>
                         </div>
-                    )
-                })}
+                    </div>
+                </div>
             </div>
         );
     }
 }
-
-
 class Movielist extends Component {
-    constructor() {
-        super();
-        this.state = {
-            show: true
-        }
-    }
-
     render() {
-        const {data} = this.props;
-        return <MovieItem data={data}/>
+        const {data, onMovieAdd, moviesWillWatch} = this.props;
+        return(
+            <div className="row">
+                {this.props.data.map( (item) => <MovieItem key={item.id} moviesWillWatch={moviesWillWatch} onMovieAdd={onMovieAdd} item={item}/> )}
+            </div>
+        )
     }
 }
 class MovieListWillWatch extends Component {
     render() {
+        const {moviesWillWatch} = this.props;
         return <div>
-            <p>Will Watch: 0 movies</p>
+            <p>Will Watch: {this.props.moviesWillWatch.length} movies</p>
             <ul className="list-group">
-                <li className="list-group-item">Cras justo odio</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Morbi leo risus</li>
-                <li className="list-group-item">Porta ac consectetur ac</li>
-                <li className="list-group-item">Vestibulum at eros</li>
+                {this.props.moviesWillWatch.map((item) => {
+                    return <li key={item.id}  className="list-group-item">
+                        {item.original_title}
+                    <span>{item.vote_average}</span>
+                    </li>
+                })}
             </ul>
         </div>
     }
 }
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            moviesWillWatch: []
+    state = {
+        moviesWillWatch: []
+    };
+    isToWillWatch(movieid) {
+        if(this.state.moviesWillWatch.every((item) => item.id !== movieid.id)){
+            this.setState({moviesWillWatch: [...this.state.moviesWillWatch, movieid]})
+        }else {
+            this.setState({moviesWillWatch: this.state.moviesWillWatch.filter((item) => {return item.id !== movieid.id})})
         }
-    }
-
+    };
     render() {
         return <div className="container">
             <div className="row mt-4">
                 <div className="col-9">
-                    <Movielist data={moviesData}/>
+                    <Movielist data={moviesData} moviesWillWatch={this.state.moviesWillWatch} onMovieAdd={this.isToWillWatch.bind(this)}/>
                 </div>
                 <div className="col-3">
-                    <MovieListWillWatch/>
+                    <MovieListWillWatch data={moviesData} moviesWillWatch={this.state.moviesWillWatch}/>
                 </div>
             </div>
         </div>;
